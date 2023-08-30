dbb272b1476f797b6092ef128cf96428b6b97ee6
yali <yusufali0147@hotmail.com>
2023-08-30 23:38:05 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("UsluSayiHesaplamaProgrami");
+        int n,k,toplam=1 ;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Sayı giriniz : ");
+        n=inp.nextInt();
+        System.out.print("Üssü giriniz : ");
+        k=inp.nextInt();
+
+        for(int i=1 ;i<=k;i++){
+            toplam*=(n);
+        }System.out.println("("+n+"^"+k+")"+" Sonucu : "+ toplam);
     }
 }
